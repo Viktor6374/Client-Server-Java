0e3d223f3fd849356b0282b0665e543d9e256668
Viktor Subbotin <zxcvbnmh123456ghh@mail.ru>
2023-05-01 16:46:56 +0300

    fix: for githab

--- lab3.2/Service/src/main/java/ru/subbotin/Application.java
+++ lab3.2/Service/src/main/java/ru/subbotin/Application.java
@@ -1,12 +1,9 @@
 package ru.subbotin;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ru.subbotin.dao.DaoRepositoryCats;
 import ru.subbotin.dao.DaoRepositoryOwners;
 
 @SpringBootApplication()
