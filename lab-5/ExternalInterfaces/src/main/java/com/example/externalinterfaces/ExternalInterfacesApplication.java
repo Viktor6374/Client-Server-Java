cdfccde82bf3304acbb4919fa1864bd156e74972
Viktor Subbotin <zxcvbnmh123456ghh@mail.ru>
2023-10-01 23:25:11 +0300

    add: applications

---
+++ lab-5/ExternalInterfaces/src/main/java/com/example/externalinterfaces/ExternalInterfacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalinterfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalInterfacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalInterfacesApplication.class, args);
+    }
+
+}
---
+++ lab-5/ExternalInterfaces/src/test/java/com/example/externalinterfaces/ExternalInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
